a995ce008dcded6eccd73db46967ec7e15854fe9
peteruithoven <peter@peteruithoven.nl>
2015-10-26 11:18:39 +0100

    Prefilter data
    - Filter based on category
    - Show days or hours units

--- src/app.js
+++ src/app.js
@@ -12,13 +12,14 @@ export default class App extends React.Component {
     var file = files[0];
     this._readFile(file)
       .then(this._parseFile)
-      //.catch((e) => alert(e));
+      .then(this._filterFile)
+      .catch((e) => console.log(e));
   }
   _readFile = (file) => {
     return new Promise((resolve, reject) => {
       var reader = new FileReader();
       reader.addEventListener('load', (event) => {
-        file.contents = event.target.result;
+        file.rawContent = event.target.result;
         resolve(file);
       }, false );
       if ( reader.readAsBinaryString !== undefined ) {
@@ -34,24 +35,10 @@ export default class App extends React.Component {
 
       switch(extension) {
         case 'csv':
-        let result = csv.parse(file.contents, {header: true});
-          console.log('result: ', result);
-          let backlog = {};
-          for(let item of result.data) {
-            console.log('item: ', item);
-            let category = 'category-'+item.Categorie.toLowerCase()
-              .replace(/(\+|^\s|\s&)/g,'')
-              .replace(/\s+/g,'-');
-            // console.log('category: ', category);
-            if(backlog[category] === undefined) {
-              backlog[category] = {
-                title: item.Categorie,
-                items: []
-              };
-            }
-            backlog[category].items.push(item);
-          }
-          this.setState({backlog});
+          var result = csv.parse(file.rawContent, {header: true});
+          // console.log('result: ', result);
+          file.content = result.data;
+          resolve(file);
           break;
         default:
           reject(`Unsupported file format (${extension}).`)
@@ -59,9 +46,49 @@ export default class App extends React.Component {
       }
     });
   }
+  _filterFile = (file) => {
+    return new Promise((resolve, reject) => {
+      var data = file.content
+      let backlog = {};
+      for(let item of data) {
+        // console.log('item: ', item);
+        let category = item.Categorie.toLowerCase()
+          .replace(/(\+|^\s|\s&)/g,'');
+        // console.log('category: ', category);
+        if(category === '') continue;
+        category = 'category-'+category
+          .replace(/\s+/g,'-');
+        if(backlog[category] === undefined) {
+          backlog[category] = {
+            title: item.Categorie,
+            items: []
+          };
+        }
+        item = this._filterItem(item);
+        backlog[category].items.push(item);
+      }
+      this.setState({backlog});
+    });
+  }
+  _filterItem = (rawItem) => {
+    var time;
+    var days = rawItem['Man dagen'];
+    if(days != undefined) {
+      if(days < 1) time = days*8+'u';
+      else time = days+'d';
+    }
+    return {
+      category: rawItem.Categorie,
+      priority: rawItem.Prioriteit,
+      status: rawItem.Status,
+      time: time||rawItem.Tijd,
+      title: rawItem.Titel,
+      story: rawItem.Verhaal
+    }
+  }
   render () {
     const {backlog} = this.state;
-    console.log('backlog: ', backlog);
+    // console.log('backlog: ', backlog);
     return (
       <FileDropZone onDrop={this.onDrop}>
         {Object.keys(backlog).map((categoryID) => {
@@ -76,12 +103,7 @@ export default class App extends React.Component {
                 return <BacklogItem 
                   key={index}
                   categoryID={categoryID}
-                  category={item.Categorie}
-                  priority={item.Prioriteit}
-                  status={item.Status}
-                  time={item.Tijd}
-                  title={item.Titel}
-                  story={item.Verhaal}/>
+                  {...item} />
               })}
             </div>
           )
@@ -90,5 +112,4 @@ export default class App extends React.Component {
     );
   }
 }
-
 ReactDOM.render(<App />, document.getElementById('app'));
